68f9c50f607e957825293e32b7a386df27530dc7
Kopyl Tatyana <yurtochka@yandex.ru>
2022-12-15 00:26:36 +0300

    add Example005 ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "люба")
+{
+    Console.WriteLine("Ура, это Люба");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
